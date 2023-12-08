db4ec406ed50cf301d152cc13b00b2a1c0c58af7
Frida <139373483+FridaKristian@users.noreply.github.com>
2023-12-08 11:37:55 +0100

    Backend wasnt saved - try again

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import Task from '../models/Task';
+import User from '../models/User';
+
+
 const router = express.Router();
 
-router.get('/tests', async (req, res) => {
-  res.send('HELLOooooo');
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
